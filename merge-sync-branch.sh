2fd12416c13d0ff72299f94cf312ab3465ef688e
Lee Blackburn <g.lee.blackburn@gmail.com>
2020-06-24 16:49:33 -0400

    starting

---
+++ merge-sync-branch.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+src_branch=$1
+if [ -z "$src_branch" ]; then
+  echo "Please specify a source branch"
+  exit 1
+fi
+
+dst_branch=$2
+if [ -z "$dst_branch" ]; then
+  echo "Please specify a destination branch"
+  exit 2
+fi
+
+# Ensure working directory in version branch clean
+git update-index -q --refresh
+if ! git diff-index --quiet HEAD --; then
+  echo "Working directory not clean, please commit your changes first"
+  exit
+fi
