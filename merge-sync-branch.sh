ef8868197bcd82d8e9bb86257f9a156bf8b637ec
Lee Blackburn <g.lee.blackburn@gmail.com>
2020-06-24 16:53:21 -0400

    checkout destination branch

--- merge-sync-branch.sh
+++ merge-sync-branch.sh
@@ -18,3 +18,9 @@ if ! git diff-index --quiet HEAD --; then
   echo "Working directory not clean, please commit your changes first"
   exit
 fi
+
+# Get current branch and checkout if needed
+branch=$(git symbolic-ref --short -q HEAD)
+if [ "$branch" != "$dst_branch" ]; then
+  git checkout $dst_branch
+fi
